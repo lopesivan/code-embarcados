eb20e36f43d63125d4d4b985998e390b3db11245
Ivan Lopes <ivan (at) 42algoritmos (dot) com (dot) br>
2017-11-28 22:38:46 -0200

    Adiciona o arquivo integer.h

---
+++ integer.h
@@ -0,0 +1,13 @@
+#ifndef INTEGER_H
+#define INTEGER_H
+
+typedef int                  Scalar;
+
+#define data_no_fmt          "%d"
+
+#define data_fmt             "%2d" /*default: %8d*/
+
+#define atos                 atoi
+
+#endif       /* INTEGER_H */
+
