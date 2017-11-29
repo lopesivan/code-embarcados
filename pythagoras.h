82a6e1af16a1d861aad2a1ffa01ac17597b1387b
Ivan Lopes <ivan (at) 42algoritmos (dot) com (dot) br>
2017-11-28 22:38:47 -0200

    Adiciona o arquivo pythagoras.h

---
+++ pythagoras.h
@@ -0,0 +1,15 @@
+#ifndef PYTHAGORAS
+#define PYTHAGORAS
+
+#include "except.h"
+#include "datatype.h"
+
+/*****************************************************************************
+ *                                                                           *
+ * ---------------------------- Public Interface --------------------------- *
+ *                                                                           *
+ *****************************************************************************/
+
+Scalar pythagoras(Scalar a, Scalar b);
+
+#endif       /* PYTHAGORAS */
