728d38d12342ab8f8b81e4eb6e298a11eafdab97
Ivan Lopes <ivan (at) 42algoritmos (dot) com (dot) br>
2017-11-28 22:38:46 -0200

    Adiciona o arquivo ltype.h

---
+++ ltype.h
@@ -0,0 +1,20 @@
+#ifndef LTYPE_H
+#define LTYPE_H
+
+#include "except.h"
+#include "datatype.h"
+
+
+/*****************************************************************************
+ *                                                                           *
+ * ---------------------------- Public Interface --------------------------- *
+ *                                                                           *
+ *****************************************************************************/
+
+typedef Scalar list_t;
+/* typedef char* list_t; */
+
+#define list_data_fmt          data_fmt
+
+#endif 		/* LTYPE_H */
+/* -*- vim: set ts=4 sw=4 tw=78 ft=header: -*- */
